57b7a6c3839b9cf12bae9adcff4fd8aa3ba19d01
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2021-11-18 12:52:17 +0100

    db config

--- config/database.js
+++ config/database.js
@@ -1,24 +1,26 @@
 const { parse } = require('pg-connection-string');
-console.log(process.env.DATABASE_URL);
-const config = parse(process.env.DATABASE_URL);
 
-module.exports = () => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({ env }) => {
+  const config = parse(env('DATABASE_URL'));
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
+        options: {},
       },
-      options: {},
     },
-  },
-});
+  };
+};
