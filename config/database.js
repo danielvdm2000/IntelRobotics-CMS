7c70c4cfc21ebb0138f1575682143a704030a279
Daniel van der Meijden <danielvandermeijden@hotmail.com>
2021-11-18 11:48:13 +0100

    log db url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
-const parse = require('pg-connection-string').parse;
+const { parse } = require('pg-connection-string');
+console.log(process.env.DATABASE_URL);
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = () => ({
